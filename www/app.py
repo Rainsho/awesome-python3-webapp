d7f956032d7a857273e213949d40bc3c15866574
Rainsho <djkyux@163.com>
2016-04-23 02:53:42 +0800

    day7, base is OK, handlers for design

--- www/app.py
+++ www/app.py
@@ -110,7 +110,7 @@ def datetime_filter(t):
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def init(loop):
-    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www', password='www', db='awesome')
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www-data', password='www-data', db='awesome')
     app = web.Application(loop=loop, middlewares=[
         logger_factory, response_factory
     ])
